cd030f15cd80df9d807f8ab95049a83e5caeb0e1
Yahya-ADLOUNI <adlouniyahya14@gmail.com>
2024-10-05 22:48:22 +0200

    updated training 2

--- training.sh
+++ training.sh
@@ -1,5 +1,17 @@
 cp -r training/matthew MeloTTS/melo/data/
 
-cd MeloTTS/melo && python3 preprocess_text.py --metadata data/matthew/metadata.list --config_path configs/config.json
+cp -a MeloTTS/melo/* .
+
+python3 preprocess_text.py --metadata data/matthew/metadata.list --config_path configs/config.json
 
 #cd MeloTTS/melo && python3 MeloTTS/melo/train.py MeloTTS/melo/data/matthew/config.json 10
+
+
+
+
+
+
+
+
+!cd MeloTTS/melo/data && ls matthew
+!cd MeloTTS/melo && python train.py -c data/matthew/config.json -m matthew --local_rank 0 --world-size 1
